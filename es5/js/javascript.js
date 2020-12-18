ccd7b3073fd30d3b99c1475ef25264fc018948ee
Federico <crudofederico@gmail.com>
2020-12-18 14:31:44 +0100

    es 5 complete

--- es5/js/javascript.js
+++ es5/js/javascript.js
@@ -1,26 +1,14 @@
-//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby
-// chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
-var nome=prompt("Inserisci il nome");
-nome=nome.charAt(0).toUpperCase()+ nome.slice(1).toLowerCase();
-var lista=["Luca", "Marco", "Paolo"];
-var messaggio=false;
-//caricamento dati
-     if(lista.indexOf(nome)==-1){
+//L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+var primaParola=prompt("Inserisci la prima parola") ;
+var secondaParola=prompt("Inserisci la seconda parola") ;
 
-         document.getElementById("message").innerHTML='<span id="red">Il tuo nome non è nella lista </span>';
-      }
-      else{
-         document.getElementById("message").innerHTML='<span id="green">Il tuo nome  è nella lista';
-      }
-     
-    //  for (var i = 0; i < lista.length; i++){
-    //     if (lista[i]==nome){  
-    //         messaggio=true;
-    //     }
-
-    // }
-    // if (messaggio == true){
-    //     document.getElementById("message").innerHTML='<span id="green">Il tuo nome  è nella lista';
-    // }   else {
-    //     document.getElementById("message").innerHTML='<span id="red">Il tuo nome non è nella lista </span>';
-    // }
+//controllo numero maggiore
+if(primaParola.length>secondaParola.length){
+   document.getElementById("message").innerHTML='La prima  immessa è piu lunga  '+primaParola+'  '+secondaParola;
+   }
+else if(primaParola.length<secondaParola.length){
+   document.getElementById("message").innerHTML='La seconda immessa è piu lunga  '+secondaParola+'  '+primaParola;
+}
+else{
+   document.getElementById("message").innerHTML='Stampate secondo l ordine di immissione:  '+primaParola+'  '+secondaParola;
+}
