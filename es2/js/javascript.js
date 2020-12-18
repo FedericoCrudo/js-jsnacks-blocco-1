4b2a2306a93704e16bd091a05469840de60bbd6d
Federico <crudofederico@gmail.com>
2020-12-18 11:22:00 +0100

    es2 update

--- es2/js/javascript.js
+++ es2/js/javascript.js
@@ -3,12 +3,24 @@ var numeri;
 var numeridisp=[];
 
 //caricamento dati
-    for(var i=0; i<6;i++){
-        numeri=parseInt(prompt("Inserisci numero")) ;
-        if(numeri%2 !=0){
-            numeridisp[i]=numeri;
+     for(var i=0; i<6;i++){
+         numeri=parseInt(prompt("Inserisci numero")) ;
+         if(numeri%2 !=0){
+         numeridisp[i]=numeri;
         }
 
     }
+    console.log(numeridisp);
 
- console.log(numeridisp);
+    // // secondo metodo
+    // var numeri=[];
+    // for(var i=0; i<6;i++){
+    //     numeri[i]=parseInt(prompt("Inserisci numero")) ;
+    //     if(numeri[i]%2 == 0){
+    //         numeri.splice(i);
+    //     }
+
+    // }
+
+
+    //console.log(numeri);
