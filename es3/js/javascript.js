e1ee2e5d3523686818c70a03447870d476814aa6
Federico <crudofederico@gmail.com>
2020-12-18 14:14:13 +0100

    es 3 alternative method

--- es3/js/javascript.js
+++ es3/js/javascript.js
@@ -3,13 +3,24 @@
 var nome=prompt("Inserisci il nome");
 nome=nome.charAt(0).toUpperCase()+ nome.slice(1).toLowerCase();
 var lista=["Luca", "Marco", "Paolo"];
-
+var messaggio=false;
 //caricamento dati
      if(lista.indexOf(nome)==-1){
 
-        document.getElementById("message").innerHTML='<span id="red">Il tuo nome non è nella lista </span>';
-     }
-     else{
-        document.getElementById("message").innerHTML='<span id="green">Il tuo nome  è nella lista';
-     }
+         document.getElementById("message").innerHTML='<span id="red">Il tuo nome non è nella lista </span>';
+      }
+      else{
+         document.getElementById("message").innerHTML='<span id="green">Il tuo nome  è nella lista';
+      }
+     
+    //  for (var i = 0; i < lista.length; i++){
+    //     if (lista[i]==nome){  
+    //         messaggio=true;
+    //     }
 
+    // }
+    // if (messaggio == true){
+    //     document.getElementById("message").innerHTML='<span id="green">Il tuo nome  è nella lista';
+    // }   else {
+    //     document.getElementById("message").innerHTML='<span id="red">Il tuo nome non è nella lista </span>';
+    // }
