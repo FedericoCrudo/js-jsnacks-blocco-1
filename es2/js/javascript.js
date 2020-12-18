61815d917ec70cfa0d888e73e3232079a67a3b7f
Federico <crudofederico@gmail.com>
2020-12-18 10:55:34 +0100

    update

---
+++ es2/js/javascript.js
@@ -0,0 +1,14 @@
+//Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
+var numeri;
+var numeridisp=[];
+
+//caricamento dati
+    for(var i=0; i<6;i++){
+        numeri=parseInt(prompt("Inserisci numero")) ;
+        if(numeri%2 !=0){
+            numeridisp[i]=numeri;
+        }
+
+    }
+
+ console.log(numeridisp);
